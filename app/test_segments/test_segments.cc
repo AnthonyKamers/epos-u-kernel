9d986d8d4284ba1d6cdf1b40c4415250d749cc1a
juliogramos <juliogoncalvesramos@yahoo.com.br>
2023-05-09 18:25:00 -0300

    corrige detacher e revisa testes

--- app/test_segments/test_segments.cc
+++ app/test_segments/test_segments.cc
@@ -28,54 +28,37 @@ int main()
     cout << "  extra segment 1 ("<< es1->pt() << ") => " << ES1_SIZE << " bytes, done!" << endl;
     cout << "  extra segment 2 ("<< es2->pt() << ") => " << ES2_SIZE << " bytes, done!" << endl;
 
-    for(int i = 0; i < 8; i++)
-        cout << "PD[" << i << "] = " << hex << a[i] << endl;
-
     cout << "Attaching segments:" << endl;
     CPU::Log_Addr * extra1 = self.attach(es1);
     CPU::Log_Addr * extra2 = self.attach(es2);
     cout << "  extra segment 1 => " << extra1 << " done!" << endl;
     cout << "  extra segment 2 => " << extra2 << " done!" << endl;
 
-    for(int i = 0; i < 8; i++)
-        cout << "PD[" << i << "] = " << hex << a[i] << endl;
+    
+   cout << "Clearing segments:";
+   memset(extra1, 0, ES1_SIZE);
+   memset(extra2, 0, ES2_SIZE);
+   cout << "  done!" << endl;
 
-    long *x = (long *) es1->pt();
-    cout << "PT extra segment 1:"<< endl;
-    for(int i = 0; i < 3; i++)
-        cout << "PT[" << i << "] = " << hex << x[i] << endl;
+   cout << "Writing on segment 1:";
+   *extra1 = 0xdeadbeef;
+   cout << "  done!" << endl;
+   cout << "Extra segment 1 address = " << hex << extra1 << endl;
+   cout << "Extra segment 1 value = " << hex << *extra1 << endl;
 
-    long *z = (long *) es2->pt();
-    cout << "PT extra segment 2:"<< endl;
-    for(int i = 0; i < 3; i++)
-        cout << "PT[" << i << "] = " << hex << z[i] << endl;
+   cout << "Detaching segments:";
+   self.detach(es1);
+   self.detach(es2);
+   cout << "  done!" << endl;
 
-//    Não Funciona
-//    
-//    cout << "Clearing segments:";
-//    memset(extra1, 0, ES1_SIZE);
-//    memset(extra2, 0, ES2_SIZE);
-//    cout << "  done!" << endl;
-//
-//    cout << "Writing on segment 1:";
-//    *extra1 = 0xdeadbeef;
-//    cout << "  done!" << endl;
-//    cout << "Extra segment 1 address = " << hex << extra1 << endl;
-//    cout << "Extra segment 1 value = " << hex << *extra1 << endl;
-//
-//    cout << "Detaching segments:";
-//    self.detach(es1);
-//    self.detach(es2);
-//    cout << "  done!" << endl;
-//
-//    for(int i = 0; i < 8; i++)
-//        cout << "PD[" << i << "] = " << hex << a[i] << endl;
-//
-//    cout << "Deleting segments:";
-//    delete es1;
-//    delete es2;
-//    cout << "  done!" << endl;
-//
+   for(int i = 0; i < 8; i++)
+       cout << "PD[" << i << "] = " << hex << a[i] << endl;
+
+   cout << "Deleting segments:";
+   delete es1;
+   delete es2;
+   cout << "  done!" << endl;
+
     cout << "I'm done, bye!" << endl;
 
     return 0;
