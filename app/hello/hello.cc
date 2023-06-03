7a4b26b5627d5f6e5e36011b9171c77e5ee3e809
juliogramos <juliogoncalvesramos@yahoo.com.br>
2023-06-03 15:24:29 -0300

    teste 6 threads

--- app/hello/hello.cc
+++ app/hello/hello.cc
@@ -3,31 +3,31 @@
 #include <real-time.h>
 #include <synchronizer.h>
 #include <process.h>
-#include <utility/math.h>
 
 using namespace EPOS;
 
 const unsigned int iterations = 5;
 
-const unsigned int period_a = 4; // ms
-const unsigned int period_b = 5; // ms
-const unsigned int period_c = 6; // ms
-const unsigned int period_d = 11; // ms
+const unsigned int period_a = 20; // ms
+const unsigned int period_b = 40; // ms
+const unsigned int period_c = 50; // ms
+const unsigned int period_d = 120; // ms
+const unsigned int period_e = 800; // ms
+const unsigned int period_f = 1200; // ms
 
-const unsigned int deadline_a = 3; // ms
-const unsigned int deadline_b = 4; // ms
-const unsigned int deadline_c = 5; // ms
-const unsigned int deadline_d = 10; // ms
+const unsigned int deadline_a = 15; // ms
+const unsigned int deadline_b = 40; // ms
+const unsigned int deadline_c = 50; // ms
+const unsigned int deadline_d = 80; // ms
+const unsigned int deadline_e = 400; // ms
+const unsigned int deadline_f = 1000; // ms
 
-const unsigned int capacity_a = 1; // ms
-const unsigned int capacity_b = 1; // ms
-const unsigned int capacity_c = 2; // ms
-const unsigned int capacity_d = 1; // ms
-
-const unsigned int wcet_a = 1; // ms
-const unsigned int wcet_b = 1; // ms
-const unsigned int wcet_c = 1; // ms
-const unsigned int wcet_d = 1; // ms
+const unsigned int capacity_a = 15; // ms
+const unsigned int capacity_b = 5; // ms
+const unsigned int capacity_c = 5; // ms
+const unsigned int capacity_d = 10; // ms
+const unsigned int capacity_e = 5; // ms
+const unsigned int capacity_f = 10; // ms
 
 OStream cout;
 Chronometer chrono;
@@ -35,9 +35,10 @@ Periodic_Thread * thread_a;
 Periodic_Thread * thread_b;
 Periodic_Thread * thread_c;
 Periodic_Thread * thread_d;
+Periodic_Thread * thread_e;
+Periodic_Thread * thread_f;
 
 int work_thread(int number) {
-    cout << "Thread " << number << " started." << endl;
     do {
         Microsecond elapsed = chrono.read() / 1000;
         cout << "T = " << elapsed << ". Thread " << number << " working" << endl;
@@ -56,6 +57,8 @@ int main()
     thread_b = new Periodic_Thread(RTConf(period_b * 1000, deadline_b * 1000, capacity_b * 1000, 0, iterations), &work_thread, 2);
     thread_c = new Periodic_Thread(RTConf(period_c * 1000, deadline_c * 1000, capacity_c * 1000, 0, iterations), &work_thread, 3);
     thread_d = new Periodic_Thread(RTConf(period_d * 1000, deadline_d * 1000, capacity_d * 1000, 0, iterations), &work_thread, 4);
+    thread_e = new Periodic_Thread(RTConf(period_e * 1000, deadline_e * 1000, capacity_e * 1000, 0, iterations), &work_thread, 5);
+    thread_f = new Periodic_Thread(RTConf(period_f * 1000, deadline_f * 1000, capacity_f * 1000, 0, iterations), &work_thread, 6);
 
     chrono.start();
 
@@ -63,6 +66,8 @@ int main()
     thread_b->join();
     thread_c->join();
     thread_d->join();
+    thread_e->join();
+    thread_f->join();
 
     chrono.stop();
 
